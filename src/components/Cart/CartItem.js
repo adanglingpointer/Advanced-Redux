ae3eef63b26f1adc5ffd1309cc50fdc230dbd6be
adanglingpointer <dave@imail.cloud>
2024-03-18 21:46:28 -0400

    updating cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,4 +1,6 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+
+import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
   const { title, quantity, total, price } = props.item;
@@ -8,7 +10,7 @@ const CartItem = (props) => {
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
